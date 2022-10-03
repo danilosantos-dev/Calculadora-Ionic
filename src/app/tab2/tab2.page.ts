4e294e3ef47487f229cfea0b67e5d11e1fe6208a
Danilo Santos <danilo.santos513@etec.sp.gov.br>
2022-10-03 20:30:56 -0300

    Aplicando métodos nos botões

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -7,6 +7,27 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
+  operacao ='';
+  resultado='';
+
   constructor() {}
 
+  adicionarValor(valor: any){
+    this.operacao += valor;
+  }
+
+  limparOperacao(){
+    this.operacao="";
+  } 
+
+  limparMemoria(){
+    this.operacao="";
+    this.resultado="";
+  }
+
+  apagarCaracter(){
+    if(this.operacao.length > 0){
+    this.operacao = this.operacao.substring(0, this.operacao.length - 1);  
+    }
+  }
 }
