ebb8065aa652872f656c6fac2831f0b3d9a48dc1
Danilo Santos <danilo.santos513@etec.sp.gov.br>
2022-10-17 20:32:50 -0300

    Adicionando validacoes

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { evaluate } from 'mathjs';
 
 @Component({
   selector: 'app-tab2',
@@ -9,11 +10,34 @@ export class Tab2Page {
 
   operacao ='';
   resultado='';
-
+  numero = false;
+  caracter = true;
+  caracteres = ['.' , '/', '*', '+', '-'];
+  
   constructor() {}
 
-  adicionarValor(valor: any){
-    this.operacao += valor;
+  realizarOperacao(){
+
+    try{
+      this.resultado = evaluate(this.operacao);
+    } catch (err){
+      this.resultado = 'Inválido!';
+    }
+  }
+
+  adicionarValor(valor: string){
+    this.caracter = this.caracteres.includes(valor);
+
+    //se não for caracter especial, numero recebe true e é adicionado;
+
+    if(!this.caracter){
+      this.operacao += valor;
+      this.numero = true;
+    }else if (this.caracter && this.numero ){
+      this.operacao += valor;
+      this.numero = false;
+    }
+
   }
 
   limparOperacao(){
@@ -29,5 +53,20 @@ export class Tab2Page {
     if(this.operacao.length > 0){
     this.operacao = this.operacao.substring(0, this.operacao.length - 1);  
     }
+
+     const ultimo = this.operacao.substring(this.operacao.length , 1);
+     this.caracter = this.caracteres.includes(ultimo);
+
+     console.log(ultimo);
+
+     // valida o ultimo caracter, se for caracter especial não se repete;
+
+     if(!this.caracter){
+       this.numero = true;
+     } else {
+       this.numero = false;
+     }
   }
+
+
 }
